35ec91305d7656152ab5c99e7563559179930147
Santiago Pereira <s.pereira.insua@gmail.com>
2025-02-24 19:45:17 -0300

    Conexión con DB

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import dotenv from "dotenv"
 import express from "express"
 import http from "http"
 import { Server } from "socket.io"
@@ -5,6 +6,9 @@ import { engine } from "express-handlebars"
 import productsRouter from "./routes/products.router.js"
 import cartsRouter from "./routes/carts.router.js"
 import viewsRouter from "./routes/views.router.js"
+import connectMongoDB from "./db/db.js"
+
+dotenv.config()
 
 const PORT = 8080
 const app = express()
@@ -27,5 +31,7 @@ app.set("views", "./src/views")
 app.use("/api/products", productsRouter)
 app.use("/api/carts", cartsRouter)
 app.use("/", viewsRouter(io))
+
+connectMongoDB()
 
 server.listen( PORT , () => console.log("Servidor iniciado en: http://localhost:"+PORT))
